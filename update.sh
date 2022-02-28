84bcaf0c4dd663f228f77db83a141ad4d491920a
Ryan Pereira <ryan.pereira@ibm.com>
2022-02-28 15:30:20 +1100

    Remove old file

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm calendar.ics
 wget 'https://outlook.office365.com/owa/calendar/4ffd79978cf04c469c5dcf83a16c3eff@ibm.com/75e705935072408083eba2a395b34623475859516253854388/calendar.ics'
 python transform.py
 curl -vvv -u 'ryan:<password>' -X PUT 'http://192.168.1.103:5232/ryan/Work' --data-binary @calendar.ics
