aa106603049c0501212cd840499745c46f973c92
Ryan Pereira <ryan.pereira@ibm.com>
2022-02-28 15:44:33 +1100

    Update Save Path

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ wget 'https://outlook.office365.com/owa/calendar/4ffd79978cf04c469c5dcf83a16c3ef
 
 python3 transform.py
 
-curl -vvv -u 'ryan:<password>' -X PUT 'http://192.168.1.103:5232/ryan/Work' --data-binary @calendar.ics
+curl -vvv -u 'ryan:<password>' -X PUT 'http://192.168.1.103:5232/ryan/75e705935072408083eba2a395b34623475859516253854388' --data-binary @calendar.ics
